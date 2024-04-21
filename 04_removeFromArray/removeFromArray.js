475d22325058a206ec3e28aa7d62f3cc58638f4e
GavinHemmingson <GavinHemmingson@gmail.com>
2024-04-21 12:55:31 -0500

    removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) 
+{ let newArray = [];
+  for(let nums of args)
+    { 
+        console.log(nums)
 
+        for(let i = 0; i<array.length;i++)
+            {
+                if(array[i] === nums)
+                  {
+                    array.splice(i,1)
+                    i--
+                  }
+                    
+                                  
+            }
+    }
+
+  return array
 };
 
 // Do not edit below this line
