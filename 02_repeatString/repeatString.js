4cdb955ce0f97ffc8f69cb2b79b43d80543911d8
GavinHemmingson <GavinHemmingson@gmail.com>
2024-04-20 22:13:33 -0500

    repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,21 @@
-const repeatString = function() {
+const repeatString = function(x,y) {
+    let string = "";
+    if(y < 0)
+    {
+        return 'ERROR'
+    }
+    if(y == 0)
+    {
+        return ''
+    }
+    else
+    {
+        for(let i = 0; i < y; i++)
+            {
+                string = string + x;
+            }
+        return string;
+    }
 
 };
 
