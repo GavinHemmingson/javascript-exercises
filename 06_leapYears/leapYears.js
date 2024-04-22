ae5591b4bc1799e38d2a5fc695cd8ee600935ab8
GavinHemmingson <GavinHemmingson@gmail.com>
2024-04-21 19:50:53 -0500

    leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(x) 
+{
+    if(x%4 == 0 && x%400 == 0)
+    {
+        return true;
+    }
+    else if(x%4 == 0 && x%100 == 0 && x%400 != 0)
+    {
+        return false;
+    }
+    else if(x%4 == 0 && x%100 != 0)
+        return true;
+
+    else 
+        return false;
+
+
 
 };
 
