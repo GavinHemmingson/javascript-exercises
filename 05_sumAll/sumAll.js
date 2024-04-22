ad022bdd45cd360d645a12a3b8411ab199401b2c
GavinHemmingson <GavinHemmingson@gmail.com>
2024-04-21 19:08:19 -0500

    sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x, y) 
+{
 
+    if(x >= 0 && typeof x == "number" && typeof y == "number")
+    {
+        if(x>y)
+            {
+                let z = x;
+                x=y;
+                y=z;
+            }
+
+        let start = x;
+        let answer = 0;
+        for(let i = x; i < y+1;i++)
+            {
+                answer = answer + i;
+            }
+        return answer
+    }
+    else 
+    return "ERROR"
 };
 
 // Do not edit below this line
