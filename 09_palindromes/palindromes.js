b161098dd255af33a7dd98633085e12d1907cc38
GavinHemmingson <GavinHemmingson@gmail.com>
2024-04-22 23:09:00 -0500

    palindromes.js

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,35 @@
-const palindromes = function () {
+const palindromes = function (x) 
+{
+    let string = x;
+    string = string.toLowerCase();
+    string = string.split("");
+    let newString = [];
+    let stringRev = [];
 
+    for(let i = 0; i < string.length;i++)
+        {
+            if (string[i].charCodeAt() >= 48 && string[i].charCodeAt() <= 57 ||
+                string[i].charCodeAt() >= 65 && string[i].charCodeAt() <= 90 ||
+                 string[i].charCodeAt() >= 97 && string[i].charCodeAt() <= 122)
+                {
+                    newString.push(string[i]);
+                }
+        }
+        
+    for(let i = newString.length -1; i >= 0; i--)
+        {
+            stringRev.push(newString[i])
+        }
+
+    for(let i = 0; i < newString.length; i++)
+        {
+            if(stringRev[i].charCodeAt() != newString[i].charCodeAt())
+                {
+                    return false
+                }
+        }
+
+return true
 };
 
 // Do not edit below this line
