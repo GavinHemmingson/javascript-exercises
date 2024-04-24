c4bc4d885f7bedc2f37533678b46cfa85aa9f106
GavinHemmingson <GavinHemmingson@gmail.com>
2024-04-23 23:29:18 -0500

    fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) 
+{
+    let sum = 0;
+    let lastNum = 1;
+    let beforeNum = 0;
+    x  = parseInt(x);
+    if(x < 0)
+    {
+        return "OOPS"
+    }
+    //0 1 2 3 4 5 6
+    //0 1 1 2 3 5 8
+    if(x == 1)
+        {
+            sum = 1;
+        }
+    for(let i = 1; i < x; i++)
+    {
+        sum = lastNum + beforeNum
+        beforeNum = lastNum
+        lastNum = sum
+        
+    }
+    return sum
 };
 
 // Do not edit below this line
