4f9797a0a4e770365cb171d93c5687417669cedc
GavinHemmingson <GavinHemmingson@gmail.com>
2024-04-25 22:10:38 -0500

    ../11_getTheTitles/getTheTitles.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(x) 
+{  
+    return x.map(getBookTitle)
 
 };
 
+function getBookTitle(x)
+{
+    return x.title;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
